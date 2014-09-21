a08dd23b59ac412e5f2e6de2f0a398bb31aa8a04
Herman Hiddema <herman@hiddema.nl>
2014-09-22 01:03:34 +0200

    Clean up csv

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,5 +79,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => 'morning-tor-1888.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'morning-tor-1888.herokuapp.com' }
 end
