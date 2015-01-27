1f5466f0b913dabfa1a1e6f69d1f2d43dafe8f0c
Herman Hiddema <herman@hiddema.nl>
2015-01-27 11:19:08 +0100

    Only show participants for current season

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,28 +1,20 @@
 class ParticipantsController < ApplicationController
   before_action :set_participant, only: [:show, :edit, :update, :destroy]
 
-  # GET /participants
-  # GET /participants.json
   def index
-    @participants = Participant.all.includes(:club).order('rating DESC')
+    @participants = @season.participants.includes(:club).order('rating DESC')
   end
 
-  # GET /participants/1
-  # GET /participants/1.json
   def show
   end
 
-  # GET /participants/new
   def new
     @participant = Participant.new
   end
 
-  # GET /participants/1/edit
   def edit
   end
 
-  # POST /participants
-  # POST /participants.json
   def create
     @participant = Participant.new(participant_params)
     if @participant.season_id.blank?
@@ -40,8 +32,6 @@ class ParticipantsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /participants/1
-  # PATCH/PUT /participants/1.json
   def update
     respond_to do |format|
       if @participant.update(participant_params)
@@ -54,8 +44,6 @@ class ParticipantsController < ApplicationController
     end
   end
 
-  # DELETE /participants/1
-  # DELETE /participants/1.json
   def destroy
     @participant.destroy
     respond_to do |format|
@@ -65,12 +53,10 @@ class ParticipantsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_participant
       @participant = Participant.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def participant_params
       params.require(:participant).permit(:firstname, :lastname, :rating, :egd_pin, :club_id, :season_id)
     end
