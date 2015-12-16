ad6855155957dcf338fdc8f4090db6da8febdf19
Herman Hiddema <herman@hiddema.nl>
2015-12-16 10:09:06 +0100

    Improve result table code

--- app/models/season.rb
+++ app/models/season.rb
@@ -13,8 +13,15 @@ class Season < ActiveRecord::Base
 
 	def results
 		res = Hash.new
-		participants.select { |p| p.played_games.reject(&:forfeit?).length > 0 }.sort_by { |p| p.rating_change || 0 }.reverse.map { |p| res[p.id] ||= Array.new }
-		games.reject(&:forfeit?).each do |game|
+		leagues.order(:order).includes(:teams).each do |league|
+			league.teams.sort_by(&:placement_criteria).reverse.each do |team|
+				team.team_members.includes(:participant).order(:board_number).map(&:participant).each do |participant|
+					res[participant.id] ||= Array.new
+				end
+			end
+		end
+		participants.select { |p| p.played_games.length > 0 }.sort_by { |p| p.rating_change || 0 }.reverse.map { |p| res[p.id] ||= Array.new }
+		games.each do |game|
 			res[game.black_id] ||= Array.new
 			res[game.white_id] ||= Array.new
 			free = 0
@@ -26,24 +33,29 @@ class Season < ActiveRecord::Base
 			games.each_with_index do |game, j|
 				if game.nil?
 					res[pid][j] = [nil, nil, '=', 0]
+				elsif game[0].forfeit?
+					res[game[1]][j] << 0
 				else
 					res[game[1]][j] << i+1
 				end
 			end
 		end
 		width = res.map { |k,v| v.length }.max
-		res.map.with_index do |(pid,games), i|
+		player_list = res.map.with_index do |(pid,games), i|
 			p = participants.find(pid)
-			("#{i+1}\t" + 
-			"%-30s" % (p.lastname + ' ' + p.firstname) + "\t" +
-			p.rank + "\t" + "NL" + "\t" + p.club.abbrev + "\t" +
-			"%.2f" % (100 * p.rating_change) + "\t" +
-			games.select { |g| "#{g[2]}" == '+' }.length.to_s + "\t" +
-			games.map do |game|
-				"#{game[3]}#{game[2]}"
-			end.join("\t") + "\t" +
-			(games.length...width).map { "0=" }.join("\t")).strip
+			[
+				i+1, 
+				"%-30s" % [p.lastname, p.firstname].map(&:strip).join(' '),
+				p.rank,
+				"NL",
+				p.club.abbrev,
+				(100 * p.rating_change).to_s[0..5],
+				games.select { |game| "#{game[2]}" == '+' }.length,
+				games.map { |game| "#{game[3]}#{game[2]}" },
+				(games.length...width).map { "0=" }
+			].flatten.map(&:to_s).join("\t")
 		end
+		teams_list = leagues.order(:order).map { |league| league.teams.sort_by(&:placement_criteria).reverse }.flatten + [OpenStruct.new(name: "Reserves")]
+		player_list.each_slice(3).zip(teams_list).map { |p,t| [t ? "# #{t.name}" : nil, p] }.flatten.compact
 	end
-
 end
